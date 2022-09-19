fee618b9ee76ed31f7852b505f41e55af2e5dda9
Felix Ostermann <ostermann-felix@web.de>
2022-09-19 10:37:54 +0200

    app setup

---
+++ GlobalStyle.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: arial;
+    margin: 0;
+  }
+`;
+
+export default GlobalStyle;
